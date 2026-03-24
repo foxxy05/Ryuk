f56274f255833e3be0972903a9a9ef51d6e32048
Atreya Rahegaonkar <atreyaxr@gmail.com>
2026-03-24 23:15:07 +0530

    nucleo_omni_i2c code import
    imported the code by vidhani for better structuring the code for easier debugging, rather messy code

---
+++ 1000_NUCLEO_OMNI_I2C/Core/Inc/Encoderlib.h
@@ -0,0 +1,27 @@
+/*
+ * Encoderlib.h
+ *
+ *  Created on: Mar 7, 2026
+ *      Author: HP
+ */
+
+#ifndef INC_ENCODERLIB_H_
+#define INC_ENCODERLIB_H_
+
+#include "stdio.h"
+#include "stdint.h"
+#include "main.h"
+
+typedef struct{
+
+	int16_t velocity;
+	int64_t position;
+	uint32_t last_counter_value;
+    uint8_t initialized;
+
+}encoder_instance;
+
+void update_encoder(encoder_instance *encoder_value ,TIM_HandleTypeDef *htim);
+void reset_encoder(encoder_instance *encoder_value);
+
+#endif /* INC_ENCODERLIB_H_ */
