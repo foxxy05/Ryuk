59c611277b5a7da1a262b139c5531e61a8d7935e
Atreya Rahegaonkar <atreyaxr@gmail.com>
2026-03-31 18:35:58 +0530

    comm file init
    added libraries folder and comm project

---
+++ 3000_COMMUNICATION_ONLY/Core/Inc/PCA9685.h
@@ -0,0 +1,29 @@
+/*
+ * PCA9685.h
+ *
+ *  Created on: Dec 23, 2
+ *  025
+ *      Author: OM
+ */
+
+#ifndef PCA9685_H
+#define PCA9685_H
+#include "stdio.h"
+#include "stm32f4xx.h"
+#include <main.h>
+#include <stdint.h>
+
+typedef struct{
+
+	I2C_HandleTypeDef *hi2c;
+	uint16_t frequency;
+	uint8_t address;
+
+}PCA_t;
+
+void PCA_init(PCA_t *pca,I2C_HandleTypeDef *hi2c, uint16_t frequency);
+void rotate(PCA_t *pca ,uint8_t channel, uint8_t angle);
+
+#endif /* INC_PCA9685_H_ */
+
+
